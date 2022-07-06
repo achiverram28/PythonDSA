40ab5e897ea17c11a5eda579ed5bbb0a58678408
achiverram28 <97288756+achiverram28@users.noreply.github.com>
2022-07-06 18:52:12 +0530

    Create createstack.py

---
+++ createstack.py
@@ -0,0 +1,9 @@
+stack=[] #empty stack
+##push opeartions 
+stack.append('a')
+stack.append('b')
+stack.append('c')
+stack.append('d')
+print("Initial Stack")
+print(stack)
+print(stack.pop())#pop operation
